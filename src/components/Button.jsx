80987b90131d248cef058e80161270b98eb88f70
Dan Nguyen <nguyen.danimal@gmail.com>
2023-01-13 11:04:48 -0600

    button component complete

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
-export const Button = () => {
+import styles from '../styles/Global'
+
+export const Button = ({ assetUrl, link }) => {
   return (
-    <div>Button</div>
+    <div 
+      className={styles.btnBlack}
+      onClick={() => window.open(link, "_blank")}
+    >
+      <img src={assetUrl} alt="expo_icon" className={styles.btnIcon} />
+      <div className="flex flex-col justify-startml-10">
+        <p className={`${styles.btnText} font-normal text-xs`}>View it on</p>
+        <p className={`${styles.btnText} font-bold text-sm`}>Expo Store</p>
+      </div>
+    </div>
   )
 }
